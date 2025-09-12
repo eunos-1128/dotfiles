adea7cc92a520b02d77471eb737a7bf866bdd191
UENO, M. <eunosium-1128@live.jp>
2025-09-12 16:24:47 +0000

    config.fish: update to use fastfetch and improve other tools initialization

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,44 @@
 if status --is-interactive
-   neofetch
+   fastfetch
 end
 
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-status --is-interactive; and source (nodenv init -|psub)
+# set PATH so it includes user's private bin if it exists
+set -g fish_user_paths "/usr/local/sbin" "$HOME/.pixi/bin" $fish_user_paths
 
+# asdf
 source /usr/local/opt/asdf/libexec/asdf.fish
 
-# local::libを使用する設定
-eval (perl -I$HOME/.perl5/lib/perl5 -Mlocal::lib=$HOME/.perl5)
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /usr/local/Caskroom/miniforge/base/bin/conda
+    eval /usr/local/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/usr/local/Caskroom/miniforge/base/etc/fish/conf.d/conda.fish"
+        . "/usr/local/Caskroom/miniforge/base/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/usr/local/Caskroom/miniforge/base/bin" $PATH
+    end
+end
 
-# 環境変数の設定
+if test -f "/usr/local/Caskroom/miniforge/base/etc/fish/conf.d/mamba.fish"
+    source "/usr/local/Caskroom/miniforge/base/etc/fish/conf.d/mamba.fish"
+end
+# <<< conda initialize <<<
+
+# Added by Windsurf
+fish_add_path /Users/masa_u/.codeium/windsurf/bin
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+# pnpm
+set -gx PNPM_HOME "/Users/masa_u/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+# perl local::lib
+eval (perl -I$HOME/.perl5/lib/perl5 -Mlocal::lib=$HOME/.perl5)
 set -x PERL_MM_OPT "INSTALL_BASE=$HOME/.perl5"
 set -x PERL_MB_OPT "--install_base $HOME/.perl5"
 set -x PERL5LIB "$HOME/.perl5/lib/perl5:$PERL5LIB"
